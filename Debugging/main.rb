e15fb3185995fd978fde73bb2617c683d548049c
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-10 14:17:29 -0600

    Notes for Class

---
+++ Debugging/main.rb
@@ -0,0 +1,14 @@
+string = 'Random String'
+
+puts strin
+
+Did you mean string
+
+def area(width = 2, height = 4)
+width * height
+end
+
+puts area('2', '6')
+
+No implicit conversion of string to integer.
+
