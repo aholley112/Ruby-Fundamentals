4c7f17a4bc21b9d5e60e88376f3bebef3682d65b
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-12 20:48:46 -0600

    Notes for Class

---
+++ USA-Covid-19-CLI-Web-Scraping-US-States/lib/cli.rb
@@ -0,0 +1,71 @@
+require_relative 'scraper.rb'
+require_relative 'country.rb'
+require_relative 'state.rb'
+
+
+class CLI
+  # run
+
+  def run
+    Scraper.scrape_data
+    greeting
+   menu 
+  end_program
+  end
+
+  # greeting
+
+  def greeting
+    puts "Welcome to the USA Covid 19 Tracker"
+  end
+
+  # end program
+
+  def end_program
+    puts "Thank you for using the USA Covid 19 Tracker"
+  end
+
+  # menu
+  def menu
+    list_options
+    input = gets.chomp.downcase
+    choose_option(input)
+    input 
+  end
+
+  # list of options
+
+  def list_options
+    puts "Please select an option:"
+    puts "1. List all states "
+    puts "2. List top ten states with the most confirmed covid cases"
+    puts "3. Print usa information"
+    puts  "Exit the program by entering 'exit'"
+  end
+
+  def choose_option(option)
+    case option
+      when "1"
+      puts "Listing all states.."
+      State.all.each do |state|
+        puts "-------"
+        puts "Name: #{state.name}"
+        puts "Cases: #{state.confirmed_cases}"
+        puts "Deaths: #{state.overall_deaths}" 
+        puts "Recoveries: #{state.recoveries}"
+        puts "-------"
+        
+      end
+      when "2"
+      puts "Listing top ten states with the most confirmed cases"
+
+      when "3"
+      puts "Printing usa information.."
+      country = Country.first
+      puts country.name
+      puts country.confirmed_cases
+      puts country.overall_deaths
+      puts country.recoveries
+    end
+  end
+end
