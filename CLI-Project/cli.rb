a0fca5ae8e63d3953a8b82a1575687458b91788a
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-17 17:58:02 -0600

    Update cli.rb

--- CLI-Project/cli.rb
+++ CLI-Project/cli.rb
@@ -24,5 +24,57 @@ class CLI
     end
   end
 
+def display_results(books)
+    
+    puts "\nFound the following books:"
+    books.each_with_index do |book, index|
+      puts "#{index + 1}. Title: #{book[:title]}"
+    end
+  end
 
+  def user_actions
+    puts "1. Sign Up\n2. Login\n3. Exit"
+    choice = gets.chomp.to_i
+
+    case choice
+    when 1
+      sign_up
+    when 2
+      login
+    when 3
+      exit
+    else
+      puts "Invalid choice."
+      user_actions
+    end
+  end
+
+  def sign_up
+    puts "Enter username:"
+    username = gets.chomp
+    puts "Enter password:"
+    password = gets.chomp
+    User.new(username, password)  
+    puts "Signed up successfully!"
+    user_actions  
+  end
+
+  def login
+    puts "Enter username:"
+    username = gets.chomp
+    puts "Enter password:"
+    password = gets.chomp
+
+    user = User.authenticate(username, password)
+    if user
+      @current_user = user
+      puts "Logged in successfully!"
+      true  
+    else
+      puts "Invalid username or password."
+      false  
+      user_actions  
+    end
+  end
+end
   
