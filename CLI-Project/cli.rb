caed1354978db8794524379937c320199dc7c38d
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-17 17:57:43 -0600

    Update cli.rb

--- CLI-Project/cli.rb
+++ CLI-Project/cli.rb
@@ -1,24 +1,28 @@
+require_relative 'user'  
+
 class CLI
-api = Api.new
-    puts "Welcome to the Goodreads CLI!"
+  def initialize
+@api = Api.new  
+  end
 
-    puts "Do you prefer science fiction, mystery, or non-fiction?"
-    print "Enter your choice: "
-    genre = gets.chomp.strip.downcase  
+  def start
+    user_actions  
 
-    until ["science fiction", "mystery", "non-fiction"].include?(genre)
-      puts "Invalid choice. Please choose from science fiction, mystery, or non-fiction."
+    if @current_user
+      puts "Welcome to the Goodreads CLI!"
+      puts "Do you prefer science fiction, mystery, or non-fiction?"
       print "Enter your choice: "
-      genre = gets.chomp.strip.downcase  
-    end
+      genre     = gets.chomp.strip.downcase  
 
-    display_results(api.fetch_books(genre))
+      until ["science fiction", "mystery", "non-fiction"].include?(genre)
+        puts "Invalid choice. Please choose from science fiction, mystery, or non-fiction."
+        print "Enter your choice: "
+        genre = gets.chomp.strip.downcase  
+      end
+
+      display_results(@api.fetch_books(genre))  
+    end
   end
 
-  def display_results(books)
-    puts "\nFound the following books:"
-    books.each_with_index do |book, index|
-      puts "#{index + 1}. Title: #{book[:title]}, Author: #{book[:author]}"
-    end
-  end
-end
+
+  
