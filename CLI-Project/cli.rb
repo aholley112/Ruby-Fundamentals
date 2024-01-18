ad4969e9a8a3e4d4006de9407a35cb2875c2277e
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-18 14:35:06 -0600

    Update cli.rb

--- CLI-Project/cli.rb
+++ CLI-Project/cli.rb
@@ -2,53 +2,65 @@ require_relative 'user'
 
 class CLI
   def initialize
-@api = Api.new  
+    @api = Api.new  
   end
 
   def start
-    user_actions  
-
-    if @current_user
-      puts "Welcome to the Goodreads CLI!"
-      puts "Do you prefer science fiction, mystery, or non-fiction?"
-      print "Enter your choice: "
-      genre     = gets.chomp.strip.downcase  
-
-      until ["science fiction", "mystery", "non-fiction"].include?(genre)
-        puts "Invalid choice. Please choose from science fiction, mystery, or non-fiction."
-        print "Enter your choice: "
-        genre = gets.chomp.strip.downcase  
-      end
-
-      display_results(@api.fetch_books(genre))  
-    end
-  end
-
-def display_results(books)
-    
-    puts "\nFound the following books:"
-    books.each_with_index do |book, index|
-      puts "#{index + 1}. Title: #{book[:title]}"
-    end
+    user_actions
   end
 
   def user_actions
-    puts "1. Sign Up\n2. Login\n3. Exit"
+  if @current_user
+    puts "1. Choose Genre\n2. Change Password\n3. Logout\n4. Exit"
     choice = gets.chomp.to_i
 
     case choice
     when 1
-      sign_up
+      choose_genre
     when 2
-      login
+      change_password
     when 3
+      logout
+    when 4
       exit
     else
       puts "Invalid choice."
       user_actions
     end
+  else
+
+      puts "1. Sign Up\n2. Login\n3. Exit"
+      choice = gets.chomp.to_i
+
+      case choice
+      when 1
+        sign_up
+      when 2
+        login
+      when 3
+        exit
+      else
+        puts "Invalid choice."
+        user_actions
+      end
+    end
   end
 
+  def choose_genre
+    puts "Welcome to the Goodreads CLI!"
+    puts "Do you prefer science fiction, mystery, or non-fiction?"
+    print "Enter your choice: "
+    genre = gets.chomp.strip.downcase  
+
+    until ["science fiction", "mystery", "non-fiction"].include?(genre)
+      puts "Invalid choice. Please choose from science fiction, mystery, or non-fiction."
+      print "Enter your choice: "
+      genre = gets.chomp.strip.downcase  
+    end
+
+    display_results(@api.fetch_books(genre))
+  end
+  
   def sign_up
     puts "Enter username:"
     username = gets.chomp
@@ -69,12 +81,24 @@ def display_results(books)
     if user
       @current_user = user
       puts "Logged in successfully!"
-      true  
+     user_actions 
     else
-      puts "Invalid username or password."
-      false  
+      puts "Invalid username or password."  
       user_actions  
+    end
+     end
+  
+    def logout
+      @current_user = nil
+      puts "Logged out successfully."
+      user_actions  
+  end
+
+  def display_results(books)
+    puts "\nFound the following books:"
+    books.each_with_index do |book, index|
+      puts "#{index + 1}. Title: #{book[:title]}"
     end
   end
 end
-  
+
