7f54f523d750133d3b42383e52e31c1f9c2a4163
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-19 19:42:25 -0600

    Update main.rb

--- Technical-Interview-Questions/main.rb
+++ Technical-Interview-Questions/main.rb
@@ -130,3 +130,25 @@ end
 
   highest_key = high(high_scores)
   puts "The key with the highest value is: #{highest_key}"
+
+    # Exercise 11
+
+  # A hash is nested within another hash. Write code that will return the value 30.
+
+  # hash = {
+  #   :a => {
+  #     :b => 30
+  #   },
+  #   :c => {
+  #     :d => 40
+  #   },
+  #   :e => {
+  #     :f => {
+  #       :g => 50
+  #     }
+  #   }
+  # }
+
+  value = hash[:a][:b]
+
+  puts "The value is: #{value}"
