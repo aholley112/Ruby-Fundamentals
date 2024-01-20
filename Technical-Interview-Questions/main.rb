50727f8141a950a1083d385a4f54f565a7a2a330
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-20 09:38:16 -0600

    Update main.rb

--- Technical-Interview-Questions/main.rb
+++ Technical-Interview-Questions/main.rb
@@ -180,4 +180,18 @@ end
   product = multiply(a, b)
   puts "The product of #{a} and #{b} is: #{product}"
 
+# Exercise 14
+
+# Create a method called divisible_by_three that takes an array of numbers and returns a new array containing only those numbers that are divisible by three. Use the divisible_by_three method to find the numbers that are divisible by three in the following array:
+
+# numbers = [9, 3, 1, 8, 6, 15, 12]
+
+def divisible_by_three(numbers)
+  numbers.select { |number| number % 3 == 0 }
+end
+
+numbers = [9, 3, 1, 8, 6, 15, 12]
+
+divisible_numbers = divisible_by_three(numbers)
+puts "Numbers divisible by three are: #{divisible_numbers}"
 
