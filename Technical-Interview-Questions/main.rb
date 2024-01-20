b662aa5a130b376f997709c59c2a099d50dd4340
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-19 19:32:14 -0600

    Update main.rb

--- Technical-Interview-Questions/main.rb
+++ Technical-Interview-Questions/main.rb
@@ -97,3 +97,18 @@ end
 string_to_reverse = "hello world"
 reversed_string = reverse(string_to_reverse
 puts "The reversed string is: #{reversed_string}"
+
+# Exercise 9
+  
+# Create a method called find_longest_word that takes an array of words and returns the length of the longest one. Use the find_longest_word method to find the length of the longest word in the following array:
+# words = ["hello", "what", "is", "up", "dude"]
+
+  def find_longest_word(words)
+    longest_word = words.max_by { |word| word.length }
+    longest_word.length
+  end
+
+  words = ["hello", "what", "is", "up", "dude"]
+
+  longest_word_length = find_longest_word(words)
+  puts "The length of the longest word is: #{longest_word_length}"
