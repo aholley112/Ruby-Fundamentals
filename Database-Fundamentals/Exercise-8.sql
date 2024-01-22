14db8e2d54908b15ab356ddd4ed1619f882a4643
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2024-01-22 14:02:10 -0600

    Add files via upload

---
+++ Database-Fundamentals/Exercise-8.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Products (
+    ProductID int,
+    ProductName text,
+    Price float,
+    Category text
+);
+
+INSERT INTO Products (ProductID, ProductName, Price, Category) VALUES (1, 'Apple', 0.50, 'Fruit');
+INSERT INTO Products (ProductID, ProductName, Price, Category) VALUES (2, 'Bread', 1.50, 'Bakery');
+
+SELECT AVG(Price) AS AveragePrice
+FROM Products;
